89a08570b44cd9b92aa1ce030e4a0f51757b0cee
Foma-droid <90155479+Foma-droid@users.noreply.github.com>
2021-09-28 23:15:05 +0300

    Працуе

--- Assets/Coinbeh.cs
+++ Assets/Coinbeh.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 
 public class Coinbeh : MonoBehaviour
 { 
-    public float hp = 1;
+    private float hp = 1;
     public float currenthp;
     public float damage = 1;
     public TMP_Text hptext;
+
+    private float lastTimeHit;
     // Start is called before the first frame update
     void Start()
     {
-       
+        
+        hptext.text = hp.ToString();
+        
     }
 
     public void MinusHp()
     {
-        currenthp = hp - damage;
-        hptext.text = currenthp.ToString();
-        if (currenthp == 0)
+        var currentTime = Time.realtimeSinceStartup;
+
+        if(lastTimeHit == 0 || (currentTime - lastTimeHit > 1f))
+        {
+            hp --;
+            lastTimeHit = currentTime;
+        }
+        hptext.text = hp.ToString();
+        if (hp == 0)
         {
            Destroy(this.gameObject);
         }
@@ -27,6 +37,6 @@ public class Coinbeh : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        hptext.text = hp.ToString(); 
     }
 }
